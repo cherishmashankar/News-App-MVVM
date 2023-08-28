549038a6f76e693137bb625a4915988b77510fbc
cherishmashankar <cherry.sh1996@gmail.com>
2023-08-28 11:51:37 +0200

    MVVM

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SearchNewsFragment.kt
@@ -1,7 +1,100 @@
 package com.androiddevs.mvvmnewsapp.ui.fragments
 
+import android.content.ContentValues
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.adapters.NewsAdapter
+import com.androiddevs.mvvmnewsapp.ui.NewsActivity
+import com.androiddevs.mvvmnewsapp.ui.NewsViewModel
+import com.androiddevs.mvvmnewsapp.util.Resource
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.androiddevs.mvvmnewsapp.util.Constants.Companion.SEARCH_NEWS_TIME_DELAY
+import kotlinx.android.synthetic.main.fragment_search_news.etSearch
+import kotlinx.android.synthetic.main.fragment_search_news.paginationProgressBar
+import kotlinx.android.synthetic.main.fragment_search_news.rvSearchNews
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SearchNewsFragment: Fragment(R.layout.fragment_search_news) {
+
+    lateinit var viewModel: NewsViewModel
+    lateinit var newsAadpter: NewsAdapter
+
+    val TAG = "SearchNewsFragemnet"
+
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+
+        setupRecyclerView()
+
+        var job: Job? = null
+
+        etSearch.addTextChangedListener{ editable ->
+            job?.cancel()
+            job = MainScope().launch{
+            delay(SEARCH_NEWS_TIME_DELAY)
+                editable?.let{editable ->
+                    if(editable.toString().isNotEmpty()){
+                        viewModel.searchNews(editable.toString())
+                    }
+                }
+            }
+        }
+
+
+
+
+
+        viewModel.searchNews.observe(viewLifecycleOwner, Observer { response ->
+            when(response){
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let{ newsResponse ->
+                        newsAadpter.differ.submitList(newsResponse.articles)
+                    }
+                }
+
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occured: $message")
+                    }
+                }
+
+                is Resource.Loading ->{
+                    showProgressBar()
+                }
+
+
+            }
+
+        })
+    }
+
+    private fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun setupRecyclerView(){
+        newsAadpter = NewsAdapter()
+        rvSearchNews.apply {
+            adapter = newsAadpter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY = "880d10b1edcf4732ab90259c44d6c171"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
